5ba3040e8a2301af0b6f738ce8faee17ec6e87bd
Faris Sofyan Wiganda <46597032+farissofyan@users.noreply.github.com>
2024-01-23 10:06:59 +0700

    update-portfolio

--- farissofyan-portfolio/script.js
+++ farissofyan-portfolio/script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Editor Photos", "Editor Videos", "Programming", "Designer", "Gamers"],
+        strings: ["Editor Photos", "Editor Videos", "Software Specialist", "Hardware Specialist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Editor Photos", "Editor Videos", "Programming", "Designer", "Gamers"],
+        strings: ["Editor Photos", "Editor Videos", "Software Specialist", "Hardware Specialist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
